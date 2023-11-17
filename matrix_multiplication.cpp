7c406537b9867c25f4e693681cd78e675dd8d71d
Abir Ashab Niloy <113261051+Abir-Ashab@users.noreply.github.com>
2023-11-17 15:40:00 +0600

    Update matrix_multiplication.cpp
    Added time and calculated efficiency

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
+#include <bits/stdc++.h>
 
-#define N 3
-#define M 4
-#define L 4
-#define K 3
+#define N 4
+#define M 3
+#define L 3
+#define K 4
 
 void print_Resulting_Matrix(int matrix[N][K]) {
     for (int i = 0; i < N; ++i) {
@@ -17,7 +16,6 @@ void print_Resulting_Matrix(int matrix[N][K]) {
         printf("\n");
     }
 }
-
 void multiplyMatrices(int A[N][M], int B[M][K], int C[N][K], int startRow, int numRows) {
     for (int i = startRow; i < startRow + numRows; ++i) {
         for (int j = 0; j < K; ++j) {
@@ -28,7 +26,6 @@ void multiplyMatrices(int A[N][M], int B[M][K], int C[N][K], int startRow, int n
         }
     }
 }
-
 int main() {
 
     int A[N][M];
@@ -47,8 +44,20 @@ int main() {
             B[i][j] = rand() % 10;
         }
     }
-
-    printf("Matrix A:\n");
+    int D[N][K];
+    clock_t start_t = clock();
+    
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < K; ++j) {
+            D[i][j] = 0;
+            for (int k = 0; k < M; ++k) {
+                D[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    clock_t end_t = clock();
+    double exec_t = (double) (end_t - start_t)/CLOCKS_PER_SEC;
+    printf("\nMatrix A:\n");
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < M; ++j) {
             printf("%d ", A[i][j]);
@@ -63,10 +72,22 @@ int main() {
         }
         printf("\n");
     }
-    int shm_id_C = shmget(IPC_PRIVATE, N * K * sizeof(int), IPC_CREAT | 0666);
-    int* sharedC = (int*)shmat(shm_id_C, NULL, 0);
+    // Inter-Process Communication(IPC)
+    // IPC_PRIVATE : might be used with shmget to create a new shared memory segment, not about making private,Its just an uses for uniqueness.There is nothing exist like IPC_PUBLIC
+    // IPC_CREAT: Create the IPC resource if it doesn't exist.
+    // shm_id_C : used to store the shared memory segment ID for matrix C
+    // 0666(octal number) : It specifies read and write permissions for all users 
+    // IPC_CREAT | 0666 :  It is a flag which ensures that the resource is created with adequate permissions for all users
+    // sharedC : a pointer to the shared memory segment where matrix C will be stored.
 
-    int numProcesses = N;
+    key_t key = IPC_PRIVATE;
+    int shm_id_C = shmget(key, N * K * sizeof(int), IPC_CREAT | 0666);
+    int* sharedC = (int*)shmat(shm_id_C, NULL, 0);
+    clock_t start_time = clock();
+    int numProcesses;
+    int a = N, b = K;
+    if(a < b) numProcesses = b;
+    else numProcesses = a;
 
     for (int i = 0; i < numProcesses; ++i) {
         pid_t childPID = fork();
@@ -75,23 +96,29 @@ int main() {
             int numRows = 1;
             multiplyMatrices(A, B, C, startRow, numRows);
             printf("\nResulting Matrix C:\n");
-            print_Resulting_Matrix((int (*)[K]) sharedC);
+            print_Resulting_Matrix((int (*)[K]) sharedC); 
             for (int r = 0; r < numRows; ++r) {
                 for (int j = 0; j < K; ++j) {
                     sharedC[(startRow + r) * K + j] = C[startRow + r][j];
                 }
             }
+            // terminate process
             exit(0);
-        }
+        }    
     }
-
+    // used to ensure that the parent process waits for all of its child processes to complete before proceeding further.
     for (int i = 0; i < numProcesses; ++i) {
         wait(NULL);
     }
-    printf("\nResulting Matrix C:\n");
+    clock_t end_time = clock();
+    double exec_time = (double) (end_time - start_time)/CLOCKS_PER_SEC;
+    printf("\nFinal Resulting Matrix C:\n");
     print_Resulting_Matrix((int (*)[K]) sharedC);
+    printf("Execution time : %f\n", exec_time);
+    double efficiency = (exec_t - exec_time) * 100;
+    printf("Efficiency of the matrices is %f percent", efficiency);
+    // Shared Memory Cleanup
     shmdt(sharedC);
     shmctl(shm_id_C, IPC_RMID, NULL);
-
     return 0;
 }
